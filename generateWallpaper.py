1626e06a542d328260f8e5ee15aa111712fb5900
giuaig <gibiz@gmx.com>
2020-10-29 21:08:03 +0100

    Mucho mas poo colors

--- generateWallpaper.py
+++ generateWallpaper.py
@@ -148,6 +148,13 @@ colors = [
     "#2e2e2d",
   ],
   [
+    "#cbf453",
+    "#fd347d",
+    "#f98152",
+    "#a4a2a2",
+    "#f6fdfb"
+  ],
+  [
     "#780116",#UP Maroon
     "#f7b538",#Maximum Yellow Red
     "#db7c26",#Ochre
